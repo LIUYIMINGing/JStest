e2db795ca82a5023fd06b6821834729b34db7acf
LIUYIMINGing <ysw0125@126.com>
2018-07-03 16:04:49 +0800

    7.3

--- musicPlayer/gulpfile.js
+++ musicPlayer/gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require("gulp");
-    htmlClean = require("gulp-htmlclean"),
-    imageMin = require("gulp-imagemin"),
-    uglify = require("gulp-uglify"),
-    stripDebug = require("gulp-strip-debug")
+    htmlClean = require("gulp-htmlclean"),//压缩html
+    imageMin = require("gulp-imagemin"),//压缩图片
+    uglify = require("gulp-uglify"),//压缩js
+    stripDebug = require("gulp-strip-debug"),//取消js中的调试语句
+    concat = require("gulp-concat"),//合并不同js文件
+    gulpLess = require("gulp-less"),
+    postCss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),//添加前缀
+    cssNano = require("cssnano")//压缩css
+
 var folder = {
     src : "./src/",//开发目录文件夹
     dist : "./dist/"//压缩打包后的目录文件夹
@@ -21,10 +27,18 @@ gulp.task("images",function(){
 gulp.task("js",function(){
     gulp.src(folder.src + "js/*")
         .pipe(stripDebug())
+        .pipe(concat("main.js"))//将src.js/下的js全部合并到dist.js/main.js里
         .pipe(uglify())
         .pipe(gulp.dest(folder.dist + "js/"))
 })
+gulp.task("css",function(){
+    var options = [autoprefixer(),cssNano()]
+    gulp.src(folder.src + "css/*")
+        .pipe(gulpLess())
+        .pipe(postCss(options))
+        .pipe(gulp.dest(folder.dist + "css/"))
+})
 
-gulp.task("default",["html","images","js"],function(){
+gulp.task("default",["html","images","js","css"],function(){
 
 })
---
+++ musicPlayer/src/js/a.js
@@ -0,0 +1,4 @@
+function a1() {
+    console.log('aaa');
+    var a1A = 1;
+}
